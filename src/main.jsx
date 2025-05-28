1dc7725364af031a568f4fa9545af19de99c6f92
Chethanv07 <148672814+Chethanv07@users.noreply.github.com>
2025-05-28 08:54:25 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Root element where the React app will be mounted
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  const root = createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  console.error('Root element not found')
+}
